3fe9737a2097bb7c547c6649a447a637d2127e5f
Gabriel Santos <gabriel.f.santos@capgemini.com>
2023-10-04 15:12:31 -0300

    inclusão do JsonInclude(JsonInclude.Include.NON_NULL)

--- src/main/java/br/com/start/model/dto/ErroDto.java
+++ src/main/java/br/com/start/model/dto/ErroDto.java
@@ -3,6 +3,8 @@ package br.com.start.model.dto;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -10,6 +12,7 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ErroDto {
 
 	private String erro;
